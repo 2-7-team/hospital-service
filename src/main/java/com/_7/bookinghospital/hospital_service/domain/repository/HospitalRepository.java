042f60fd61bba038a2c0070ffc1679b395d94121
goodperiodt <ilypsj@gmail.com>
2025-04-10 13:02:38 +0900

    HospitalRepository 인터페이스 작성

--- src/main/java/com/_7/bookinghospital/hospital_service/domain/repository/HospitalRepository.java
+++ src/main/java/com/_7/bookinghospital/hospital_service/domain/repository/HospitalRepository.java
@@ -1,7 +1,12 @@
 package com._7.bookinghospital.hospital_service.domain.repository;
-
 import com._7.bookinghospital.hospital_service.domain.model.Hospital;
+
+import java.util.List;
+import java.util.UUID;
 
 public interface HospitalRepository {
     Hospital save(Hospital hospital);
+    Hospital findOneHospital(UUID id);
+    List<Hospital> findAllHospitals();
+    boolean existsHospital(UUID id);
 }
