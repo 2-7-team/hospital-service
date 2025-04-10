f8e67df68795cf56122105b4e02f437e0d190f31
goodperiodt <ilypsj@gmail.com>
2025-04-10 13:01:20 +0900

    infrastructure_db 연결 역할인 (인터페이스) JpaRepository 와 해당 인터페이스를 필드로 갖는 HospitalRepositoryImpl 클래스와 HospitalRepositoryImpl 클래스가 구현한 domain>repository> HospitalRepository 인터페이스 작성

--- src/main/java/com/_7/bookinghospital/hospital_service/infrastructure/repository/HospitalRepositoryImpl.java
+++ src/main/java/com/_7/bookinghospital/hospital_service/infrastructure/repository/HospitalRepositoryImpl.java
@@ -3,16 +3,42 @@ package com._7.bookinghospital.hospital_service.infrastructure.repository;
 import com._7.bookinghospital.hospital_service.domain.model.Hospital;
 import com._7.bookinghospital.hospital_service.domain.repository.HospitalRepository;
 import com._7.bookinghospital.hospital_service.infrastructure.repository.jpa.HospitalJpaRepository;
+import jakarta.ws.rs.NotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.UUID;
+
 @Repository
 @RequiredArgsConstructor
+// 도메인>레포지토리의 인터페이스 HospitalRepository 를 인프라스트럭쳐>레포지토리 에서 구현하고,
+// 인프라스트럭쳐>레포지토리>jpa 의 인터페이스(db 연결, SimpleJpaRepository 가 구현체) 를 필드로 갖는다.
 public class HospitalRepositoryImpl implements HospitalRepository {
-    private final HospitalJpaRepository jpaRepository;
+    private final HospitalJpaRepository hospitalJpaRepository;
 
     @Override
     public Hospital save(Hospital hospital) {
-        return jpaRepository.save(hospital);
+        return hospitalJpaRepository.save(hospital);
+    }
+
+    @Override
+    public Hospital findOneHospital(UUID id) {
+        return hospitalJpaRepository
+                .findById(id)
+                .orElseThrow(() -> new NotFoundException("조회하신 병원 정보가 존재하지 않습니다."));
+    }
+
+    @Override
+    public List<Hospital> findAllHospitals() {
+        List<Hospital> hospitalList = hospitalJpaRepository.findAll();
+        // 어플에 등록된 병원이 하나도 없을 때 예외 발생
+        if(hospitalList.isEmpty()) throw new NotFoundException("등록된 병원이 없어 조회하실 수 없습니다.");
+        return hospitalList;
+    }
+
+    @Override
+    public boolean existsHospital(UUID id) {
+        return hospitalJpaRepository.existsById(id);
     }
 }
--- src/main/java/com/_7/bookinghospital/hospital_service/infrastructure/repository/jpa/HospitalJpaRepository.java
+++ src/main/java/com/_7/bookinghospital/hospital_service/infrastructure/repository/jpa/HospitalJpaRepository.java
@@ -3,5 +3,8 @@ package com._7.bookinghospital.hospital_service.infrastructure.repository.jpa;
 import com._7.bookinghospital.hospital_service.domain.model.Hospital;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface HospitalJpaRepository extends JpaRepository<Hospital, Long> {
+import java.util.UUID;
+
+// DB 와 연결, JPA
+public interface HospitalJpaRepository extends JpaRepository<Hospital, UUID> {
 }
