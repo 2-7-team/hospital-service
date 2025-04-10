97c93f171eda717f66161c1e1af41110fee7a226
goodperiodt <ilypsj@gmail.com>
2025-04-11 01:15:18 +0900

    빈 순환참조 문제 발생 - 클래스명 변경으로 해결 PatientCapacityPerHourRepositoryImpl(변경 전) : PatientCapacityPerHourRepositoryAdapter(변경 후)

---
+++ src/main/java/com/_7/bookinghospital/hospital_service/infrastructure/repository/PatientCapacityPerHourRepositoryAdapter.java
@@ -0,0 +1,21 @@
+package com._7.bookinghospital.hospital_service.infrastructure.repository;
+
+import com._7.bookinghospital.hospital_service.domain.model.PatientCapacityPerHour;
+import com._7.bookinghospital.hospital_service.domain.repository.PatientCapacityPerHourRepository;
+import com._7.bookinghospital.hospital_service.infrastructure.repository.jpa.PatientCapacityPerHourJpaRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@RequiredArgsConstructor
+public class PatientCapacityPerHourRepositoryAdapter implements PatientCapacityPerHourRepository {
+
+    @Lazy
+    private final PatientCapacityPerHourJpaRepository patientCapacityPerHourJpaRepository;
+
+    @Override
+    public PatientCapacityPerHour save(PatientCapacityPerHour patientCapacityPerHour) {
+        return patientCapacityPerHourJpaRepository.save(patientCapacityPerHour);
+    }
+}
