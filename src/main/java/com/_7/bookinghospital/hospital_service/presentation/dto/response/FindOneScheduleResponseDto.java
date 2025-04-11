81a008bae606d3fd114b3d644bf9c791ce61888d
goodperiodt <ilypsj@gmail.com>
2025-04-11 15:24:05 +0900

    schedule 단건 조회 응답용 dto 생성

--- src/main/java/com/_7/bookinghospital/hospital_service/presentation/dto/response/FindOneScheduleResponseDto.java
+++ src/main/java/com/_7/bookinghospital/hospital_service/presentation/dto/response/FindOneScheduleResponseDto.java
@@ -3,11 +3,13 @@ package com._7.bookinghospital.hospital_service.presentation.dto.response;
 import com._7.bookinghospital.hospital_service.domain.model.Hospital;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.ToString;
 
 import java.time.LocalTime;
 
 @Getter
 @AllArgsConstructor
+@ToString
 public class FindOneScheduleResponseDto {
     private String name; // 병원 이름
     private LocalTime time;
