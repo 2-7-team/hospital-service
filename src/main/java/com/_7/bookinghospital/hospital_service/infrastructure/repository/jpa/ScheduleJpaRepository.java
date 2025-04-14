a7ed4362760f3f36fd6eebb245c0173608e811a3
goodperiodt <ilypsj@gmail.com>
2025-04-14 10:47:00 +0900

    (코드에 아무 영향 없음) 주석 변경

--- src/main/java/com/_7/bookinghospital/hospital_service/infrastructure/repository/jpa/ScheduleJpaRepository.java
+++ src/main/java/com/_7/bookinghospital/hospital_service/infrastructure/repository/jpa/ScheduleJpaRepository.java
@@ -5,7 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.UUID;
 
-// PatientCapacityPerHourJpaRepository 인터페이스가 JpaRepository 인터페이스를 상속함으로써
-// PatientCapacityPerHourJpaRepository 인터페이스의 구현체는 SimpleJpaRepository 클래스가 된다.
+// ScheduleJpaRepository 인터페이스가 JpaRepository 인터페이스를 상속함으로써
+// ScheduleJpaRepository 인터페이스의 구현체는 SimpleJpaRepository 객체가 된다.
 public interface ScheduleJpaRepository extends JpaRepository<Schedule, UUID> {
 }
